d2d38b6f34b073738dded065f6888bcdbeb04388
yoonezi <jeji3391@naver.com>
2023-05-22 00:10:26 +0900

    동기/비동기 rest-api 실습

---
+++ class/pages/04-01-rest-get/index.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function RestGetPage() {
+  const [title, setTitle] = useState("");
+
+  // 이벤트핸들러 함수
+  function onClickAsync() {
+    const data = axios.get("https://koreanjson.com/posts/1");
+    console.log(data);
+  }
+
+  async function onClicksync() {
+    const data = await axios.get("https://koreanjson.com/posts/1");
+    console.log(data);
+    console.log(data.data);
+    console.log(data.data.title);
+    setTitle(data.data.title);
+  }
+
+  return (
+    <>
+      <button onClick={onClickAsync}>비동기 REST-API 요청하기</button>
+      <button onClick={onClicksync}>동기 REST-API 요청하기</button>
+      <div>{title}</div>
+    </>
+  );
+}
--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import {
   Address,
   ButtonWrapper,
   Contents,
+  Error,
   ImageWrapper,
   InputWrapper,
   Label,
@@ -19,30 +21,90 @@ import {
   WriterWrapper,
   Youtube,
   Zipcode,
-  ZipcodeWrapper,
+  ZipcodeWrapper
 } from "../../../styles/emotion";
 
 export default function BoardWriteUI() {
+  const [writer, setWriter] = useState("");
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [writerError, setWriterError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [contentsError, setContentsError] = useState("");
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+    if(event.target.value !== ""){
+      setWriterError("")
+    }
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+    if(event.target.value !== ""){
+      setPasswordError("")
+    }
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+    if(event.target.value !== ""){
+      setTitleError("")
+    }
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+    if(event.target.value !== ""){
+      setContentsError("")
+    }
+  };
+
+  const onClickSubmit = () => {
+    if (!writer) {
+      setWriterError("작성자를 입력해주세요.");
+    }
+    if (!password) {
+      setPasswordError("비밀번호를 입력해주세요.");
+    }
+    if (!title) {
+      setTitleError("제목을 입력해주세요.");
+    }
+    if (!contents) {
+      setContentsError("내용을 입력해주세요.");
+    }
+    if (writer && password && title && contents) {
+        alert("게시글이 등록되었습니다.");
+    }
+  };
+
   return (
     <Wrapper>
       <Title>게시글 등록</Title>
       <WriterWrapper>
         <InputWrapper>
           <Label>작성자</Label>
-          <Writer type="text" placeholder="이름을 적어주세요." />
+          <Writer type="text" placeholder="이름을 적어주세요." onChange={onChangeWriter} />
+          <Error>{writerError}</Error>
         </InputWrapper>
         <InputWrapper>
           <Label>비밀번호</Label>
-          <Password type="password" placeholder="비밀번호를 작성해주세요." />
+          <Password type="password" placeholder="비밀번호를 작성해주세요." onChange={onChangePassword} />
+          <Error>{passwordError}</Error>
         </InputWrapper>
       </WriterWrapper>
       <InputWrapper>
         <Label>제목</Label>
-        <Subject type="text" placeholder="제목을 작성해주세요." />
+        <Subject type="text" placeholder="제목을 작성해주세요." onChange={onChangeTitle} />
+        <Error>{titleError}</Error>
       </InputWrapper>
       <InputWrapper>
         <Label>내용</Label>
-        <Contents placeholder="내용을 작성해주세요." />
+        <Contents placeholder="내용을 작성해주세요." onChange={onChangeContents} />
+        <Error>{contentsError}</Error>
       </InputWrapper>
       <InputWrapper>
         <Label>주소</Label>
@@ -55,7 +117,7 @@ export default function BoardWriteUI() {
       </InputWrapper>
       <InputWrapper>
         <Label>유튜브</Label>
-        <Youtube placeholder="링크를 복사해주세요" />
+        <Youtube placeholder="링크를 복사해주세요." />
       </InputWrapper>
       <ImageWrapper>
         <Label>사진첨부</Label>
@@ -71,7 +133,7 @@ export default function BoardWriteUI() {
         <RadioLabel htmlFor="image">사진</RadioLabel>
       </OptionWrapper>
       <ButtonWrapper>
-        <SubmitButton>등록하기</SubmitButton>
+        <SubmitButton onClick={onClickSubmit}>등록하기</SubmitButton>
       </ButtonWrapper>
     </Wrapper>
   );
