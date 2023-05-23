67e45ef2c0c41e9c8fb704945d1c5f54f06b05fd
yoonezi <jeji3391@naver.com>
2023-05-24 00:57:15 +0900

    05-03-static-routing-board-query::useQuery로 데이터를 불러오고 화면에 보여주기

---
+++ class/pages/section05/05-03-static-routing-board-query-moved/1/index.js
@@ -0,0 +1,31 @@
+// import { gql } from "apollo-boost";
+// import { useQuery } from "react-apollo";
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_BOARD = gql`
+  query {
+    fetchBoard(number: 30) {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARD);
+  console.log(data);
+
+  return (
+    <>
+      {/* 데이터가 있으면 그리고 없으면 그리지 마 */}
+      {/* 먼저, 정적인 부분을 보여주고 그 다음 데이터가 들어오면 {data.fetchBoard.~}부분을 그려준다, */}
+      <div>1번 게시글 이동이 완료되었습니다.</div>
+      {/* data?. : optional chaiging */}
+      <div>작성자 : {data && data.fetchBoard.writer} </div>
+      <div>제목 : {data?.fetchBoard.title} </div>
+      <div>내용 : {data ? data.fetchBoard.contents : "loding .."} </div>
+    </>
+  );
+}
