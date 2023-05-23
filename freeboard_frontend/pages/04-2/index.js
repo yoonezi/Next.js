945e7e70925554bdf8e6707ecf9e60ab2d3cf293
yoonezi <jeji3391@naver.com>
2023-05-23 22:52:38 +0900

    05-01-static-routing

---
+++ freeboard_frontend/pages/04-2/index.js
@@ -0,0 +1,15 @@
+// import { gql } from "apollo-boost";
+// import { useMutation } from "react-apollo";
+
+// const CREATE_BOARD = gql``;
+
+// export default function GraphqlApiBtn() {
+//   // 선언
+//   const [createBoard] = useMutation(CREATE_BOARD);
+
+//   return (
+//     <>
+//       <button>GRAPHQL-API 요청하기 </button>
+//     </>
+//   );
+// }
