29c43e3f0a9776da17743735e2256656e059d29d
yoonezi <jeji3391@naver.com>
2023-05-29 19:55:16 +0900

    06-export vs export-default

--- class/src/components/units/board/write/BoardWrite.container.js
+++ class/src/components/units/board/write/BoardWrite.container.js
@@ -3,6 +3,13 @@ import { useMutation } from "@apollo/client";
 import BoardWriteUI from "./BoardWrite.presenter";
 import { CREATE_BOARD } from "./BoardWrite.queries";
 
+// import dkdjfenk from "./BoardWrite.presenter";  -> export default로 이름 바꿔서 가져오기
+// import dkdjfenk , { apple } from "./BoardWrite.presenter";  -> export default와 export 함께 가져오기
+
+// import * as S from './BoardWrite.presenter' -> export 한번에 다 가져오기
+// S.BlueButton
+// S.RedInput
+
 export default function BoardWrite() {
   const [writer, setWrite] = useState("");
   const [title, setTitle] = useState("");
